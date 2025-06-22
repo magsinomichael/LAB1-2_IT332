3f3c125447ef5fccbf3fd7fdd121159f95cf38ca
magsinomichael <22-16399@g.batstate-u.edu.ph>
2025-06-22 14:37:09 +0800

    first commit

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Transactions;
+using System.Collections.Generic;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("Enter the total number your enrolled course");
+            string? N_course = Console.ReadLine();
+            int num1 = Convert.ToInt32(N_course);
+
+            Console.WriteLine("Enter the price of your favorite book");
+            string? F_book = Console.ReadLine();
+            int num2 = Convert.ToInt32(F_book);
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total Number Of Course: " + num1);
+            Console.WriteLine("Favorite Book: " + num2);
+        }
+    }
+}
+
---
+++ MySecondProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Transactions;
+using System.Collections.Generic;
+
+namespace MySecondProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the pieces of your apple");
+            string? input3 = Console.ReadLine();
+            double ApplePieces = Convert.ToDouble(input3);
+
+            Console.WriteLine("Enter the price of each apple");
+            string? input4 = Console.ReadLine();
+            double ApplePrices = Convert.ToDouble(input4);
+
+            double total = ApplePieces * ApplePrices;
+
+            Console.WriteLine("Total Price:  " + total.ToString("F2"));
+
+            Console.WriteLine("Your Cash: ");
+            string? input5 = Console.ReadLine();
+            double cash = Convert.ToDouble(input5);
+
+            double change = cash - total;
+            if (cash > total)
+            {
+
+                Console.WriteLine("Your Change: " + change.ToString("F2"));
+            }
+            else
+            {
+                Console.WriteLine("Payment Invalid! ");
+            }
+        }
+    }
+}
+
+
